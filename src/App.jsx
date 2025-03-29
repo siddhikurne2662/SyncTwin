5d706a48c60a5aac8cb8a446b1cddaaebafa1241
Ruchi Mulik <mulikruchi07gmail.com>
2025-03-29 11:16:42 +0530

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -265,7 +265,7 @@ function App() {
                 </Suspense>
                 <mesh rotation={[-Math.PI / 2, 0, 0]} position={[0, -0.5, 0]}>
                     <planeGeometry args={[50, 50]} />
-                    <meshStandardMaterial color="#80b918" />
+                    <meshStandardMaterial color="#55a630" />
                 </mesh>
                 <OrbitControls enableRotate enableZoom enablePan rotateSpeed={1} zoomSpeed={0.8} panSpeed={0.8} dampingFactor={0.1} minDistance={2} maxDistance={15} />
             </Canvas>
